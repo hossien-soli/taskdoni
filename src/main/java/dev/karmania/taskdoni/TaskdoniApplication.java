4f000dac716762182b6a49efd71bf9204fcbb98d
Karmania Dev <h.soli.jff@gmail.com>
2025-01-22 04:11:51 +0330

    we have a fresh spring-boot 3.4.1 application

---
+++ src/main/java/dev/karmania/taskdoni/TaskdoniApplication.java
@@ -0,0 +1,11 @@
+package dev.karmania.taskdoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdoniApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdoniApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/karmania/taskdoni/TaskdoniApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.karmania.taskdoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
